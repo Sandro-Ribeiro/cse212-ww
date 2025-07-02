b85eb658d1b5c37dfd3a4454bb3f921955ad2373
Sandro Ribeiro <144251723+Sandro-Ribeiro@users.noreply.github.com>
2025-07-02 15:17:19 -0300

    W01 - MultiplesOf function implemented

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,15 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double multiple;
+        double[] multiples = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            multiple = number * (i + 1);
+            multiples[i] = multiple;
+        }
+        return multiples;
     }
 
     /// <summary>
