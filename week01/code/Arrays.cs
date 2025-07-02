74b39307f10cb09eb25aec65e3c7ba2d483ae66a
Sandro Ribeiro <144251723+Sandro-Ribeiro@users.noreply.github.com>
2025-07-02 17:05:46 -0300

    W01 - RotateListRight() - Implemented

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel;
+using System.Data;
+using System.Linq;
 
 public static class Arrays
 {
@@ -39,5 +41,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        if (data.Any())
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                int last = data.Last();
+                for (int j = data.Count - 1; j > 0; j--)
+                {
+                    data[j] = data[j - 1];
+                }
+                data[0] = last;
+            }
+        }
+        else
+        {
+            return;
+        }
     }
 }
