3ff9ab30a15c8fa3cf5ce1607a4ab71b979d3a14
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-07-09 20:02:52 -0300

    W02 Learning Activity: Understanding Code Using Reviews

--- week02/learn/Program.cs
+++ week02/learn/Program.cs
@@ -1,9 +1,14 @@
-﻿Console.WriteLine("\n======================\nSimple Stack\n======================");
-SimpleStack.Run();
+﻿// Console.WriteLine("\n======================\nSimple Stack\n======================");
+// SimpleStack.Run();
 
-//Console.WriteLine("\n======================\nSimple Queue\n======================");
-//SimpleQueue.Run();
+// Console.WriteLine("\n======================\nSimple Queue\n======================");
+// SimpleQueue.Run();
 
 // Uncomment and run as you get to the solution part
 // Console.WriteLine("\n======================\nSimple Queue\n======================");
 // SimpleQueueSolution.Run();
+
+// Console.WriteLine("\n======================\nMysteryProgram\n======================");
+// MysteryProgram.Run();
+
+
