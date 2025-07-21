d46f383d8d1abedafd90df9310d4bafe01806341
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-07-21 12:09:45 -0300

    W03 Code: W03 Team Activity: Sets and Maps - Step 06 - Basketball

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -23,14 +23,31 @@ public class Basketball
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            }
+            else
+            {
+                players.Add(playerId, points);
+            }
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        var topPlayers = players.ToArray();
 
-        var topPlayers = new string[10];
+        Array.Sort(topPlayers, (p1, p2) => p2.Value.CompareTo(p1.Value));
+        
+        // Console.WriteLine($"Players: {{{string.Join(",\n", topPlayers)}}}\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(topPlayers[i]);
+        }
     }
 }
