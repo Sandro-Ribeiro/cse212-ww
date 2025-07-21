a9e35147244ef2c9dc457a0151b57e7cf54bc3cb
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-07-21 12:10:57 -0300

    W03 Code: W03 Team Activity: Sets and Maps - Step 06 - Basketball

--- week03/teach/Program.cs
+++ week03/teach/Program.cs
@@ -10,8 +10,8 @@
 // Console.WriteLine("\n======================\nDisplay Sums\n======================");
 // DisplaySumsSolution.Run();
 
-Console.WriteLine("\n======================\nBasketball\n======================");
-Basketball.Run();
+// Console.WriteLine("\n======================\nBasketball\n======================");
+// Basketball.Run();
 
 // Console.WriteLine("\n======================\nBasketball\n======================");
 // BasketballSolution.Run();
